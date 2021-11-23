9526430a4226ade609c4f955991ca406cc583c20
arelli <nickpap556@gmail.com>
2021-11-24 01:22:54 +0200

    somewhat working initial test

--- lab3/Assignment3/src_corpus/logger.c
+++ lab3/Assignment3/src_corpus/logger.c
@@ -9,6 +9,17 @@
 #include <sys/stat.h>
 #include <openssl/md5.h>
 
+int get_uid_of_file(const char * filename){
+	int uid;
+	uid = getuid();  // returns UID of the current(calling) process.
+	return uid;
+}
+
+
+
+
+
+
 FILE *
 fopen(const char *path, const char *mode) 
 {
@@ -21,7 +32,27 @@ fopen(const char *path, const char *mode)
 	original_fopen_ret = (*original_fopen)(path, mode);
 
 
+	/* find the original fopen */
+	FILE * file_ptr;
+	file_ptr = (*original_fopen)("log", "a");
+	/* find the original fwrite */
+	size_t (*original_fwrite)(const void*, size_t, size_t, FILE*);
+	original_fwrite = dlsym(RTLD_NEXT, "fwrite");
+	/*dummy write to the file*/
+	char * string = "abcd";
+	(*original_fwrite)(string,strlen(string),sizeof(char),file_ptr);
+	char * comma = "";
+	(*original_fwrite)(",",1,1,file_ptr);
+
+
 	/* add your code here */
+	// get UID of file
+	// Get filename
+	// Get timestamp
+	// Access type(file creation = 0, file open = 1, file write = 2)
+	// Is-Action-denied flag(user with no priviledges tried to access it)
+	// File Fingerprint(i can use md5 hash function openssl)
+
 	/* ... */
 	/* ... */
 	/* ... */
@@ -39,9 +70,17 @@ fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
 	size_t original_fwrite_ret;
 	size_t (*original_fwrite)(const void*, size_t, size_t, FILE*);
 
+
 	/* call the original fwrite function */
 	original_fwrite = dlsym(RTLD_NEXT, "fwrite");
 	original_fwrite_ret = (*original_fwrite)(ptr, size, nmemb, stream);
+
+	/* find the original fopen */
+	FILE *(*original_fopen)(const char*, const char*);
+	original_fopen = dlsym(RTLD_NEXT, "fopen");
+	FILE * file_ptr;
+	file_ptr = (*original_fopen)("log", "a");
+
 
 
 	/* add your code here */
