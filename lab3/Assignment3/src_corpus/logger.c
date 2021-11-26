c763bbb4e43cff97b3b434847ce82f24cf1b8f41
arelli <nickpap556@gmail.com>
2021-11-26 19:51:45 +0200

    added md5 and getpath functions

--- lab3/Assignment3/src_corpus/logger.c
+++ lab3/Assignment3/src_corpus/logger.c
@@ -38,8 +38,36 @@ const char* get_path_from_fp(FILE* file_ptr){
     return path;
 }
 
-const char* get_md5_from_fp(FILE * file_ptr){
-	// nop.
+/* Returns the md5 hash from the contents of the file at the specified path. 
+ * The returned value is also, self memory declared
+ */
+const char* get_md5_from_path(char* path){
+	long filesize;
+	unsigned char *buf;
+	unsigned char *hash = NULL;
+
+	FILE *original_fopen_ret;
+	FILE *(*original_fopen)(const char*, const char*);
+	original_fopen = dlsym(RTLD_NEXT, "fopen");
+
+	original_fopen_ret = (*original_fopen)(path, "rb");
+	if (original_fopen_ret==NULL){
+		printf("Could not access file to generate md5 hash... Exiting...");
+		return 0;
+	}
+	// send the cursor to the end of the file, measure length, and return it at the beginning
+	fseek(original_fopen_ret, 0, SEEK_END);
+	filesize = ftell(original_fopen_ret);
+	fseek(original_fopen_ret, 0, SEEK_SET);
+
+	buf = malloc(filesize);  // now that we know the filesize, we can allocate a buffer to hold it
+	fread(buf, filesize, 1, original_fopen_ret);
+
+	hash = malloc(MD5_DIGEST_LENGTH);  // hash length is always the same
+	MD5(buf, filesize, hash);  // MD5 is actually produced here
+	free(buf);
+	fclose(original_fopen_ret);
+	return (const char*)hash;
 }
 
 
@@ -58,33 +86,11 @@ fopen(const char *path, const char *mode)
 	original_fwrite = dlsym(RTLD_NEXT, "fwrite");
 
 
-	/* H A S I N G  A L G O R I T H M */
-	long filesize;
-	unsigned char *buf;
+	/* H A S I N G  */
 	unsigned char *hash = NULL;
-	int i;
 
-	if( access( path, F_OK ) == 0 ) {  // if file does exist...	
-		/* call the original fopen function */
-		original_fopen_ret = (*original_fopen)(path, "rb");
-		if (original_fopen_ret==NULL){
-			printf("Could not access file to generate md5 hash... Exiting...");
-			return 0;
-		}
-		// send the cursor to the end of the file, measure length, and return it at the beginning
-		fseek(original_fopen_ret, 0, SEEK_END);
-		filesize =10; //  ftell(original_fopen_ret);
-		fseek(original_fopen_ret, 0, SEEK_SET);
-
-		buf = malloc(filesize);  // now that we know the filesize, we can allocate a buffer to hold it
-		fread(buf, filesize, 1, original_fopen_ret);
-
-		hash = malloc(MD5_DIGEST_LENGTH);  // hash length is always the same
-		MD5(buf, filesize, hash);  // MD5 is actually produced here
-		free(buf);
-		fclose(original_fopen_ret);
-		/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	}
+	if( access( path, F_OK ) == 0 )   // if file does exist...	
+		hash = (unsigned char *) get_md5_from_path(path);
 	else{
 		access_type = 0;  // file creation(file did not exist)
 		hash = (unsigned char*)"0"; // md5 for empty file
@@ -97,8 +103,6 @@ fopen(const char *path, const char *mode)
 	time ( &raw_time );
   	timeinfo = localtime ( &raw_time );
 
-
-	/* write to log file*/
 
 	/* find the original fopen */
 	FILE * file_ptr;
@@ -115,7 +119,7 @@ fopen(const char *path, const char *mode)
 
 
 	sprintf(output, "uid:%d, %s, access:%d, denied:%d,", uid, (char*)get_path_from_fp(original_fopen_ret), access_type, action_denied);
-	for (i=0; i < MD5_DIGEST_LENGTH; i++){
+	for (int i=0; i < MD5_DIGEST_LENGTH; i++){
 			sprintf(output+strlen(output), "%02x",  hash[i]);
 	}
 	sprintf(output + strlen(output), ", %s", asctime(timeinfo));  // add the strlen of output, to ont overwrite the previous data
@@ -128,12 +132,11 @@ fopen(const char *path, const char *mode)
 size_t 
 fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) 
 {
+	//FILE * original_fopen_ret;
 	FILE *(*original_fopen)(const char*, const char*);
 	original_fopen = dlsym(RTLD_NEXT, "fopen");
 	int action_denied = 0;
 	int access_type =  2;  // action = file open
-
-
 
 	
 	size_t original_fwrite_ret;
@@ -150,23 +153,9 @@ fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
 
 	/************* tHe logger follows****************/
 
-	/* H A S I N G  A L G O R I T H M */
-	long filesize;
-	unsigned char *buf;
+	/* H A S I N G */
 	unsigned char *hash = NULL;
-	int i;
-
-	fseek(stream, 0, SEEK_END);
-	filesize =10; //  ftell(original_fopen_ret);
-	fseek(stream, 0, SEEK_SET);
-
-	buf = malloc(filesize);  // now that we know the filesize, we can allocate a buffer to hold it
-	fread(buf, filesize, 1, stream);
-
-	hash = malloc(MD5_DIGEST_LENGTH);  // hash length is always the same
-	MD5(buf, filesize, hash);  // MD5 is actually produced here
-	free(buf);
-	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~end of hashing~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	hash = get_md5_from_path(path);
 
 
 	/* get time */
@@ -183,8 +172,8 @@ fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
 	char * output = (char * )malloc(sizeof(char)*256);  // a lot bigger than what we need
 	int uid = getuid();
 
-	sprintf(output, "uid:%d, %s access:%d, denied:%d,", uid, path, access_type, action_denied);
-	for (i=0; i < MD5_DIGEST_LENGTH; i++){
+	sprintf(output, "uid:%d, %s, access:%d, denied:%d,", uid, path, access_type, action_denied);
+	for (int i=0; i < MD5_DIGEST_LENGTH; i++){
 			sprintf(output+strlen(output), "%02x",  hash[i]);
 	}
 	sprintf(output + strlen(output), ", %s", asctime(timeinfo));  // add the strlen of output, to ont overwrite the previous data
