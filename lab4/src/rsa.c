d93a20d2007d4b8524a508acb3c467f5cade6c18
name <nick@ohno.localdomain>
2021-12-07 13:08:02 +0200

    iifirst sieve of eratosthenes implementation, not tested.

--- lab4/src/rsa.c
+++ lab4/src/rsa.c
@@ -14,8 +14,70 @@ size_t *
 sieve_of_eratosthenes(int limit, int *primes_sz)
 {
 	size_t *primes;
+	
+	/* used to store all numbers, including primes */
+	size_t *all_numbers = (int*)malloc(sizeof(int)*limit);
+	for(int i=2;i<=limit){
+		all_numbers[i-2]=i;
+	}
 
-	/* TODO */	
+	int index = 0;
+
+	/* the step of the sieve */
+	size_t current_step = 2;
+	size_t last_step = 2;
+
+	/* used to find the next step */
+	bool not_found = true;
+	int temp_index = 0;
+
+	while(true){
+		/* there are only first numbers left in our array */
+		if (current_step*current_step > limit){
+			break;
+		}
+		
+		/* start from the beginning, increasing the sieving step */
+		if (index>=limit){
+			index = 0;
+			temp_index = 0;
+			
+			/* find the index of the current step in all_numbers[] */
+			while(true){
+				temp_index++;
+				if (all_numbers[temp_index]==current_step)
+					break;  
+				if (temp_index>limit)
+					break;  /* error state */
+			}
+
+			/* find the next non-zero number in the sieve
+			 * to serve as the next step distance        */
+			not_found = true;
+			while(not_found){
+				temp_index ++;
+				if(all_numbers[temp_index]!=0){
+					last_step = current_step;
+					current_step = all_numbers[temp_index];
+					not_found = false;
+				}
+			}
+		}
+		
+		/* set this element as non-prime, with the zero notation */
+		index += current_step; 
+		all_numbers[index] = 0;
+	}
+
+	int how_many_primes = 0;
+	for(int i; i<limit-2;i++){
+		if (all_numbers[i]!=0){
+			how_many_primes++;
+			printf("%d, \n");
+		}
+	}
+
+
 
 	return primes;
 }
