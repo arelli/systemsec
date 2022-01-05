c571f1f9b3eaba8ceed90a362ecaf2d70bda9fe3
arelli <arelli@raf-pc.localdomain>
2022-01-05 22:24:36 +0200

    kinda working

--- lab6/analyze.c
+++ lab6/analyze.c
@@ -19,13 +19,17 @@
 void print_packet_info(const u_char *packet, struct pcap_pkthdr* packet_header);
 void get_stats(pcap_t* handle, char* filter_exp);
 
-void main(){
+void main(int argc, char *argv[]){
 	char error_buffer[PCAP_ERRBUF_SIZE];
 	pcap_t *handle ;
-	handle = pcap_open_offline("test_pcap_5mins.pcap", error_buffer);
+	if ((argc<=2)||(strcmp(argv[1],"-r")!=0)){
+		printf("You need to specify -r <pcap filename>!\nExiting... \n");
+		return;
+	}
+	handle = pcap_open_offline(argv[2], error_buffer);
 	get_stats(handle,"udp || tcp");
 	pcap_close(handle);
-	
+	return;
 }
 
 
@@ -46,15 +50,19 @@ void get_stats(pcap_t* handle, char* filter_exp){
     int data_length = 0;
     int i;
     int tcp_counter = 0;
+    int tcp_bytes_counter = 0;
     int udp_counter = 0;
+    int udp_bytes_counter = 0;
+
+	/* get all services with port numbers. get_sevices.sh must be run before.
+     */
+    FILE* fp;
+    char line[256];
+    /* get_services.sh is tested only on Ubuntu 20.04.3 LTS */
+	fp = fopen("services", "r");
 
 
 	struct pcap_pkthdr packet_header;
-	/* define the neccessary variables for TCP and UDP filtering */
-	struct bpf_program filter;
-	//char filter_exp[] = "tcp || udp";
-	bpf_u_int32 subnet_mask, ip;
-
 	int packet_counter = 0;
 
 	/* this is the main loop reading packets. with each pcap_next() call,
@@ -81,14 +89,103 @@ void get_stats(pcap_t* handle, char* filter_exp){
 	         */
 	        inet_ntop(AF_INET, &(ip_header->ip_src), sourceIP, INET_ADDRSTRLEN);
 	        inet_ntop(AF_INET, &(ip_header->ip_dst), destIP, INET_ADDRSTRLEN);
-	        printf(" source IP: %s dest IP: %s", sourceIP,destIP);
 
+
+	        /* if the packet is TCP: */
 	        if(ip_header->ip_p == IPPROTO_TCP){
-	        	printf(" [TCP] \n");
+	        	/* get clear of the ethernet and ip headers */
+	        	tcp_header = (struct tcphdr*)(packet + sizeof(struct ether_header) + sizeof(struct ip));
+	        	/* find destination and source ports: */
+	        	source_port = ntohs(tcp_header->source);
+            	dest_port = ntohs(tcp_header->dest);
+
+            	int dst_prot_found=0;
+            	int src_prot_found=0;
+            	printf("type: TCP source IP: %s dest IP: %s", sourceIP,destIP);
+            	/* find protocol of source port number */
+            	while (fgets(line, sizeof(line), fp) != NULL) {
+            		char port_number[16];
+            		sprintf(port_number, "%d", source_port);
+            		/* if the port number is found in the "database"... */
+				    if (strstr(line, port_number) != NULL) {
+				    	/* the interesting offset below is to print characters only after
+				    	 * the whitespace character. this is depentend on the format of
+				    	 * our command.
+				    	 */
+				    	*(line+(strlen(line)-1))=' '; // remove trailing newline
+				        printf(" src port: %d ( %s)", source_port, line+(int)(strchr(line,' ')-line+1) );
+				        src_prot_found=1;
+				        break;
+				    }
+				}
+				if(!src_prot_found)
+					printf(" src port: %d", source_port);
+				rewind(fp);
+
+				/* find protocol of destination port number */
+				while (fgets(line, sizeof(line), fp) != NULL) {
+            		char port_number[16];
+            		sprintf(port_number, "%d", dest_port);
+				    if (strstr(line, port_number) != NULL) {
+				    	*(line+(strlen(line)-1))=' ';
+				        printf(" dst port: %d ( %s)\n", dest_port, line+(int)(strchr(line,' ')-line+1) );
+				        dst_prot_found=1;
+				        break;
+				    }
+				}
+				if(!dst_prot_found)
+					printf(" dst port: %d\n", dest_port);
+				rewind(fp);
+
 	        	tcp_counter++;
 	        }
+
+	        /* if the packet is UDP: */
 	        else if (ip_header->ip_p == IPPROTO_UDP){
-	        	printf(" UDP \n");
+	        	/* get clear of the ethernet and ip headers */
+	        	udp_header = (struct udphdr*)(packet + sizeof(struct ether_header) + sizeof(struct ip));
+
+	        	source_port = ntohs(udp_header->source);
+            	dest_port = ntohs(udp_header->dest);
+
+	        	printf("type: UDP source IP: %s dest IP: %s", sourceIP,destIP);
+
+	        	int dst_prot_found=0;
+            	int src_prot_found=0;
+            	while (fgets(line, sizeof(line), fp) != NULL) {
+            		char port_number[16];
+            		sprintf(port_number, "%d", source_port);
+            		/* if the port number is found in the "database"... */
+				    if (strstr(line, port_number) != NULL) {
+				    	/* the interesting offset below is to print characters only after
+				    	 * the whitespace character. this is depentend on the format of
+				    	 * our command.
+				    	 */
+				    	*(line+(strlen(line)-1))=' ';
+				        printf(" src port: %d ( %s) ", source_port, line+(int)(strchr(line,' ')-line+1));
+				        src_prot_found=1;
+				        break;
+				    }
+				}
+				if(!src_prot_found)
+					printf(" src port: %d", source_port);
+				rewind(fp);
+
+				/* find protocol of destination port number */
+				while (fgets(line, sizeof(line), fp) != NULL) {
+            		char port_number[16];
+            		sprintf(port_number, "%d", dest_port);
+				    if (strstr(line, port_number) != NULL) {
+				    	*(line+(strlen(line)-1))=' ';
+				        printf(" dst port: %d ( %s)\n", dest_port, line+(int)(strchr(line,' ')-line+1));
+				        dst_prot_found=1;
+				        break;
+				    }
+				}
+				if(!dst_prot_found)
+					printf(" dst port: %d\n", dest_port);
+				rewind(fp);
+
 	        	udp_counter++;
 	        }
 
@@ -99,7 +196,8 @@ void get_stats(pcap_t* handle, char* filter_exp){
 	    packet =(u_char*) pcap_next(handle, &packet_header);
 		packet_counter++;
 	}
-
+	/* close the "database" of services/ports */
 	printf("\n The total number of packets UDP(%d) or TCP(%d): %d \n ",udp_counter,tcp_counter, packet_counter);
 
 }
+
